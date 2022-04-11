b5ab4fe061c412dcba346314d87f741d6819ce4d
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-04-12 02:33:15 +0900

    initial commit - Bookmark, BookmarkList, Test

---
+++ src/Bookmark.java
@@ -0,0 +1,25 @@
+
+public class Bookmark {
+	private String name;
+	private String time;
+	private String url;
+	private String group;
+	private String memo;
+	
+	Bookmark(String name, String time, String url, String group, String memo){
+		this.name = name;
+		this.time = time;
+		this.url = url;
+		this.group = group;
+		this.memo = memo;
+	}
+	
+	
+	Bookmark(String url){
+		
+	}
+	
+	public void print() {
+		
+	}
+}
