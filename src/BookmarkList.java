a68094007568da273aeebb4ea3ac9309df6ef20f
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-04-12 18:37:20 +0900

    BookmarkList 에서 파싱, addbookmarks 구현

--- src/BookmarkList.java
+++ src/BookmarkList.java
@@ -1,4 +1,53 @@
+import java.io.File;
+import java.util.Scanner;
 
 public class BookmarkList {
-
+	private Bookmark[] bookmarkArray;
+	private int bookmarkCount;
+	
+	BookmarkList(String bookmarkFileName){
+		bookmarkArray = new Bookmark[100];
+		addBookmarks(bookmarkFileName);
+	}
+	
+	void addBookmarks(String bookmarkFileName) {
+		// 잘못된 정보 처리
+		// 1. 파일 이름 오류
+		// 2. 파싱 필수인자 오류
+		File file = new File(bookmarkFileName);
+		Scanner input = null;
+		String line = "";
+		String[] parsed;
+		try {
+			input = new Scanner(file);
+			while (input.hasNext()) {
+				line = input.nextLine().trim();
+				if (line == "" || line.substring(0, 2).equals("//")) {
+					continue;
+				}
+				else {
+					parsed = line.split("[,;]", -1); 
+					for (int i = 0; i<parsed.length; i++)
+						parsed[i] = parsed[i].trim();
+					bookmarkArray[bookmarkCount++] = new Bookmark(parsed[0], parsed[1], parsed[2], parsed[3], parsed[4]);
+				}
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			System.out.println("에러러러러");
+		}
+	}
+	
+	public int numBookmarks() {
+		return bookmarkCount;
+	}
+	
+	public Bookmark getBookmark(int i) {
+		return bookmarkArray[i];
+	}
+	
+	public void mergeByGroup() {
+		// to-do
+	}
 }
--- src/Test.java
+++ src/Test.java
@@ -6,35 +6,11 @@ public class Test {
 	public static void main(String[] args) {
 		// some test must be done in BookmarkList.java, but now here.
 		
-		File file = new File("bgbg.txt");
-		Scanner input = null;
-		String line = "";
-		String[] parsed;
-		
-		try {
-			input = new Scanner(file);
-			while (input.hasNext()) {
-				line = input.nextLine().trim();
-				if (line == "") {
-					System.out.println("빈줄");
-				}
-				else if (line.substring(0, 2).equals("//")) {
-					System.out.println("주석");
-				}
-				else {
-					System.out.println("파싱");
-					parsed = line.split("[,;]", -1); 
-					for (int i = 0; i<parsed.length; i++)
-						parsed[i] = parsed[i].trim();
-					System.out.println(parsed[0] +parsed[1] +parsed[2] + parsed[3]+parsed[4]); 
-				}
-				
-			}
-		}
-		catch(Exception e) {
-			e.printStackTrace();
-			System.out.println("에러러러러");
-		}
+
+		BookmarkList bl = new BookmarkList("bgbg.txt");
+		int num = bl.numBookmarks();
+		for (int i = 0; i<num; i++)
+			bl.getBookmark(i).print();
 		
 //		
 //		Bookmark a = new Bookmark("www.naver.com");
