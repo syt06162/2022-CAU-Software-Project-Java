dd4f4b0ec03abdab28ff071c482c29c4182145ca
DESKTOP-09KOO1A\Kwon <syt06162@naver.com>
2022-05-17 22:48:53 +0900

    GUI 1. main 메소드 주석 제거

--- src/GUITest.java
+++ src/GUITest.java
@@ -4,20 +4,7 @@ public class GUITest {
 	public static void main(String[] args) {
 
 		BookmarkManager bm = new BookmarkManager("bookmark-input.txt");
-//		
-//		// 2. BookmarkList 안에 Bookmark들 출력
-//		for (int i = 0; i<bl.numBookmarks(); i++)
-//			bl.getBookmark(i).print();
-//		
-//		System.out.println("\n");
-//		
-//		// 3. BookmarkList merge by group, 잘 되었나 확인
-//		bl.mergeByGroup();
-//		for (int i = 0; i<bl.numBookmarks(); i++)
-//			bl.getBookmark(i).print();
-//		
-//		// 4. merge 결과를 파일로도 저장해봄.
-//		bl.saveBookmarks("bookmark-output.txt");
+		
 	}
 	
 }
